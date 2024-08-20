9faed97e0daf677a2dc1f6d20e47ef1c93972897
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-08-20 04:45:03 +0200

    feat: PatchOperationFindModById

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+-   PatchOperationFindModById. Does what it says. Alternative to vanilla's PatchOperationFindMod but relies on mod id rather than on mod name, which, at least in theory, is more stable/less likely to change.
+
 ## [0.6.0] 2024-08-18
 
 ### Added
---
+++ ilyvion.Laboratory/PatchOperationFindModById.cs
@@ -0,0 +1,44 @@
+using System.Xml;
+
+namespace ilyvion.Laboratory;
+
+public class PatchOperationFindModById : PatchOperation
+{
+#pragma warning disable CS8618 // Set by reflection
+    private List<string> mods;
+
+    private PatchOperation match;
+
+    private PatchOperation nomatch;
+#pragma warning restore CS8618
+
+    protected override bool ApplyWorker(XmlDocument xml)
+    {
+        bool matched = false;
+        foreach (string mod in mods)
+        {
+            if (ModsConfig.IsActive(mod))
+            {
+                matched = true;
+                break;
+            }
+        }
+        if (matched)
+        {
+            if (match != null)
+            {
+                return match.Apply(xml);
+            }
+        }
+        else if (nomatch != null)
+        {
+            return nomatch.Apply(xml);
+        }
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}({mods.ToCommaList()})";
+    }
+}
