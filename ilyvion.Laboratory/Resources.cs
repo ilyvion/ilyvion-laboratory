7cf43c1acb35532d3e8aad8d647118082f0b10b7
Alexander Krivács Schrøder <alexschrod@gmail.com>
2025-07-19 11:00:04 +0200

    chore: remove incorrect warning

--- ilyvion.Laboratory/Resources.cs
+++ ilyvion.Laboratory/Resources.cs
@@ -2,8 +2,14 @@ namespace ilyvion.Laboratory;
 
 #pragma warning disable CS8618 // Set from mod assembly
 
+[StaticConstructorOnStartup]
 internal static class Resources
 {
+    static Resources()
+    {
+        // This is here to silence the warning about the graphic below being loaded elsewhere.
+    }
+
     // Assigned from StaticConstructorOnStartup in ilyvion.LaboratoryMod project.
     // Can't find a way to disable the stupid warning for this type, though.
     public static Texture2D GraphDot { get; set; }
